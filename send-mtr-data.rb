fdde9b2ee32581a12189f937bda73a1048611dfa
Dave Gallant <dtgallant@gmail.com>
2014-03-14 15:07:25 -0400

    adding some identifying vars to send-mtr-data

--- send-mtr-data.rb
+++ send-mtr-data.rb
@@ -4,8 +4,8 @@
 #############################################
 
 #script settings
-#host_url='http://foxtrot.orangeblueprint.com/processreport'
-host_url='http://localhost:3000/processreport'
+host_url='http://foxtrot.orangeblueprint.com/processreport'
+#host_url='http://localhost:3000/processreport'
 server_key='wqqryyhbifqtdzirlscpxkdubwamgnnqlpujwcvdrijkgdjyxn'
 
 
@@ -13,6 +13,8 @@ server_key='wqqryyhbifqtdzirlscpxkdubwamgnnqlpujwcvdrijkgdjyxn'
 `mtr --report #{ARGV[0]} > ./report_result.txt` 
 #`echo "test2 data" > ./report_result.txt` 
 
+#generate group code for results based on datestamp
+group_code = Time.now.strftime("%Y_%m_%d_%H%M%S")
 #read file into variable
 text=File.open('./report_result.txt').read
 text.each_line do |report_data_line|
@@ -25,7 +27,7 @@ text.each_line do |report_data_line|
 	cmd = "
 	/usr/bin/curl -X POST \
 	-H \"Content-Type: application/json\" \
-	--data '{\"server_key\":\"#{server_key}\",\"report_data_line\":\"#{report_data_line}\"}' \
+	--data '{\"server_key\":\"#{server_key}\",\"group_code\":\"#{group_code}\",\"host_requested\":\"#{ARGV[0]}\",\"report_data_line\":\"#{report_data_line}\"}' \
 	#{host_url}
 	"
 
